6c6ce1c0978d9f4147bf31caa54772780b6dcf9b
keilylopez22 <135383970+keilylopez22@users.noreply.github.com>
2024-03-09 13:16:02 -0600

    hi

---
+++ programa2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace programa2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola bienvenidos!");
+            Console.WriteLine("por favor ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("por favor ingrese su apellido");
+            string apellido = Console.ReadLine();
+            Console.Write("Ingrese el correo electrónico por ejemplo, gmail.com: ");
+            string correo = Console.ReadLine();
+            string GenerarCorreo = (nombre, apellido, correo);
+
+            Console.WriteLine("El correo electrónico es: {0}", GenerarCorreo);
+
+            Console.ReadLine(); 
+        }
+    }
+}
+
+
+
+
+        
+
+        
+   
